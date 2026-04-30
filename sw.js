d441a96a3e182eade2c228e56ec4025dcf4d536a
yanksu-droid <yanksu@gmail.com>
2026-04-30 21:08:35 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.map((key) => caches.delete(key)))));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) => Promise.all(keys.map((key) => caches.delete(key))))
+      .then(() => self.registration.unregister())
+      .then(() => self.clients.matchAll({ type: "window", includeUncontrolled: true }))
+      .then((clients) => {
+        clients.forEach((client) => client.navigate(client.url));
+      }),
+  );
+});
